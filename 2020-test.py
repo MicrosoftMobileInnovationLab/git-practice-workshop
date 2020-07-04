40f120866d5008035e855760cd980680f98fe503
Sumanth V Rao <sumanthvrao@gmail.com>
2020-07-04 11:57:37 +0530

    2020-test commit-fix

--- 2020-test.py
+++ 2020-test.py
@@ -2,11 +2,8 @@ import time
 
 def test(x, y):
     if(y==0):
-        return x
+        return y
     return test(y, y%x)
-
-for i in range(0, 10):
-    print('GCD of {} with 2 : '.format(i), test(2, 10))
 
 for i in range(0, 10):
     print('GCD of {} with 2 : '.format(i), test(2, 10))
